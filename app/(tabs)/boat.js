dc4cf2110573024b4aa2c8489703dd86f4240f36
pwintkooo <23029589@myrp.edu.sg>
2024-11-07 12:52:36 +0800

    L4Update

--- app/(tabs)/boat.js
+++ app/(tabs)/boat.js
@@ -1,16 +1,62 @@
-// import {Image, Text, View} from "react-native";
-// import React from "react";
-// import Icon from 'react-native-vector-icons/FontAwesome6';
-//
-// const Boat = ({title, desc, poster, icon_name}) => {
-//     return (
-//         <View>
-//             <Text style = {{fontSize: 18, fontWeight: 'bold', color:'gray'}}><Icon name={icon_name} size={20}/>{title}</Text>
-//             <Text>{desc}</Text>
-//             <Image source={poster}/>
-//             <Text>{'\n'}</Text>
-//         </View>
-//     );
-// };
-//
-// export default Boat;
+import {Image, StyleSheet, Text, View} from "react-native";
+import React from "react";
+import Icon from 'react-native-vector-icons/FontAwesome6';
+
+const styles = StyleSheet.create({
+    box: {
+        flexDirection: "column",
+        backgroundColor: '#1F3C9A',
+        borderWidth: 2,
+        borderColor: 'black',
+        marginLeft: 8,
+        marginRight: 8,
+        marginTop: 6,
+        marginBottom: 6,
+        justifyContent: "space-around",
+        alignItems: "center"
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center',
+    },
+    titleBox: {
+        borderWidth: 2,
+        backgroundColor: '#FF6F61',
+        marginTop: 10,
+        marginRight: 3,
+        marginLeft: 3,
+        padding: 5,
+        alignSelf: 'stretch',
+        flex: 1
+    },
+    descBox: {
+        borderWidth: 2,
+        backgroundColor: 'whitesmoke',
+        padding: 5,
+        textAlign: "center",
+        marginTop: 10,
+        flex: 1
+    },
+    image: {
+        marginTop: 10,
+        maxWidth: 375,
+        alignItems: "center",
+        flex: 1,
+        resizeMode: 'contain'
+    }
+})
+
+const Boat = ({title, desc, poster, icon_name}) => {
+    return (
+        <View style={styles.box}>
+            <Text style={[styles.title, styles.titleBox]}><Icon name={icon_name} size={20}/>{title}</Text>
+            <Text style={styles.descBox}>{desc}</Text>
+            <Image source={poster} style={styles.image}/>
+            <Text>{'\n'}</Text>
+        </View>
+    );
+};
+
+export default Boat;
--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,69 +1,20 @@
 import React from 'react';
 import {Text, ScrollView, Image, View, StyleSheet} from 'react-native';
 import Icon from "react-native-vector-icons/FontAwesome6";
+import Boat from './boat';
 
 const styles = StyleSheet.create({
     box: {
-        flexDirection: "column",
-        backgroundColor: '#1F3C9A',
         borderWidth: 2,
-        borderColor: 'black',
-        marginLeft: 8,
-        marginRight: 8,
-        marginTop: 6,
-        marginBottom: 6,
-        justifyContent: "space-around",
-        alignItems: "center"
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: 'white',
-        textAlign: 'center',
-    },
-    titleBox: {
-        borderWidth: 2,
-        backgroundColor: '#FF6F61',
-        marginTop: 10,
-        marginRight: 3,
-        marginLeft: 3,
-        padding: 5,
-        alignSelf: 'stretch',
-        flex: 1
-    },
-    descBox: {
-        borderWidth: 2,
-        backgroundColor: 'whitesmoke',
-        padding: 5,
-        textAlign: "center",
-        marginTop: 10,
-        flex: 1
-    },
-    image: {
-        marginTop: 10,
-        maxWidth: 375,
-        alignItems: "center",
-        flex: 1,
-        resizeMode: 'contain'
+        backgroundColor: 'grey',
     }
 })
-
-const Boat = ({title, desc, poster, icon_name}) => {
-    return (
-        <View style={styles.box}>
-            <Text style={[styles.title, styles.titleBox]}><Icon name={icon_name} size={20}/>{title}</Text>
-            <Text style={styles.descBox}>{desc}</Text>
-            <Image source={poster} style={styles.image}/>
-            <Text>{'\n'}</Text>
-        </View>
-    );
-};
 
 const AllBoats = () => {
     return (
         <ScrollView>
                 <Text>{'\n'}</Text>
-            <Text style={[styles.box, styles.title]}>GETABOAT - FOR SALE</Text>
+            <Text style={[styles.box, {fontWeight: 'bold', textAlign: 'center', fontSize: 20, color: 'white' }]}>GETABOAT - FOR SALE</Text>
             <Boat
                 title = "SEA RAY500 SUNDANCERS"
                 desc = "Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away."
